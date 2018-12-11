44bec82ab019dad4f8bccfc7522ec1bbc06c062c
Niklas Waldstein <niklaswaldstein@hilscher.com>
2018-12-11 15:44:02 +0100

    Init snippet read rotary switch.

--- SConstruct
+++ SConstruct
@@ -27,9 +27,13 @@
 SConscript('mbs/SConscript')
 Import('atEnv')
 
+# Create a build environment for the Cortex-R7 and Cortex-A9 based netX chips.
+env_cortexR7 = atEnv.DEFAULT.CreateEnvironment(['gcc-arm-none-eabi-4.9', 'asciidoc'])
+env_cortexR7.CreateCompilerEnv('NETX4000', ['arch=armv7', 'thumb'], ['arch=armv7-r', 'thumb'])
+
 # Create a build environment for the Cortex-M4 based netX chips.
 env_cortexM4 = atEnv.DEFAULT.CreateEnvironment(['gcc-arm-none-eabi-4.9', 'asciidoc'])
-env_cortexM4.CreateCompilerEnv('NETX90_MPW', ['arch=armv7', 'thumb'], ['arch=armv7e-m', 'thumb'])
+env_cortexM4.CreateCompilerEnv('NETX90', ['arch=armv7', 'thumb'], ['arch=armv7e-m', 'thumb'])
 
 # Build the platform libraries.
 SConscript('platform/SConscript')
@@ -52,39 +56,86 @@ sources = """
 	src/main.c
 """
 
+
 # The list of include folders. Here it is used for all files.
 astrIncludePaths = ['src', '#platform/src', '#platform/src/lib', '#targets/version']
 
-tEnv = atEnv.NETX90_MPW.Clone()
-tEnv.Append(CPPPATH = astrIncludePaths)
-tEnv.Replace(LDFILE = 'src/netx90/netx90_com_intram.ld')
-tSrc = tEnv.SetBuildPath('targets/netx90_com_intram', 'src', sources)
-tElf = tEnv.Elf('targets/netx90_com_intram/apply_hwconfig_netx90_com_intram.elf', tSrc + tEnv['PLATFORM_LIBRARY'])
-tTxt = tEnv.ObjDump('targets/netx90_com_intram/apply_hwconfig_netx90_com_intram.txt', tElf, OBJDUMP_FLAGS=['--disassemble', '--source', '--all-headers', '--wide'])
-tBin = tEnv.ObjCopy('targets/netx90_com_intram/apply_hwconfig_netx90_com_intram.bin', tElf)
-tTmp = tEnv.GccSymbolTemplate('targets/netx90_com_intram/snippet.xml', tElf, GCCSYMBOLTEMPLATE_TEMPLATE='targets/hboot_snippet.xml', GCCSYMBOLTEMPLATE_BINFILE=tBin[0])
+global PROJECT_VERSION
+
+
+#
+# Build the netX90 snippet.
+#
+tEnv90 = atEnv.NETX90.Clone()
+tEnv90.Append(CPPPATH = astrIncludePaths)
+tEnv90.Replace(LDFILE = 'src/netx90/netx90_com_intram.ld')
+tSrc90 = tEnv90.SetBuildPath('targets/netx90_com_intram', 'src', sources)
+tElf90 = tEnv90.Elf('targets/netx90_com_intram/apply_hwconfig_netx90_com_intram.elf', tSrc90 + tEnv90['PLATFORM_LIBRARY'])
+tTxt90 = tEnv90.ObjDump('targets/netx90_com_intram/apply_hwconfig_netx90_com_intram.txt', tElf90, OBJDUMP_FLAGS=['--disassemble', '--source', '--all-headers', '--wide'])
+tBin90 = tEnv90.ObjCopy('targets/netx90_com_intram/apply_hwconfig_netx90_com_intram.bin', tElf90)
+tTmp90 = tEnv90.GccSymbolTemplate('targets/netx90_com_intram/snippet.xml', tElf90, GCCSYMBOLTEMPLATE_TEMPLATE='targets/hboot_snippet.xml', GCCSYMBOLTEMPLATE_BINFILE=tBin90[0])
 
 # Create the snippet from the parameters.
-global PROJECT_VERSION
-aArtifactGroupReverse = ['org', 'muhkuh', 'hboot', 'sniplib']
-atSnippet = {
-    'group': '.'.join(aArtifactGroupReverse),
-    'artifact': 'apply_hwconfig_netx90_mpw',
+aArtifactGroupReverse90 = ['com', 'hilscher', 'hw', 'util', 'netx90']
+atSnippet90 = {
+    'group': '.'.join(aArtifactGroupReverse90),
+    'artifact': 'read_rotaryswitch',
     'version': PROJECT_VERSION,
-    'vcs_id': tEnv.Version_GetVcsIdLong(),
-    'vcs_url': tEnv.Version_GetVcsUrl(),
+    'vcs_id': tEnv90.Version_GetVcsIdLong(),
+    'vcs_url': tEnv90.Version_GetVcsUrl(),
     'license': 'GPL-2.0',
     'author_name': 'Muhkuh team',
     'author_url': 'https://github.com/muhkuh-sys',
-    'description': 'Apply the hardware configuration on a netX90 MPW.',
-    'categories': ['netx90MPW', 'booting'],
+    'description': 'Read rotary switches make them available on netX90',
+    'categories': ['netx90', 'booting'],
+    'parameter': {
+        'CONFIGURATION_ADDRESS': {'help': 'The address of the hardware config.', 'default': '0x0400001a'},
+        'MMIO_SELECT': {'help': 'Handover parameter for parsing mmios.'}
+    }
+
+}
+strArtifactPath90 = 'targets/snippets/%s/%s/%s' % ('/'.join(aArtifactGroupReverse90), atSnippet90['artifact'], PROJECT_VERSION)
+snippet_netx90_com = tEnv90.HBootSnippet('%s/%s-%s.xml' % (strArtifactPath90, atSnippet90['artifact'], PROJECT_VERSION), tTmp90, PARAMETER=atSnippet90)
+
+# Create the POM file.
+tPOM90 = tEnv90.POMTemplate('%s/%s-%s.pom' % (strArtifactPath90, atSnippet90['artifact'], PROJECT_VERSION), 'templates/pom.xml', POM_TEMPLATE_GROUP=atSnippet90['group'], POM_TEMPLATE_ARTIFACT=atSnippet90['artifact'], POM_TEMPLATE_VERSION=atSnippet90['version'], POM_TEMPLATE_PACKAGING='xml')
+
+
+#
+# Build the netX4000 snippet.
+#
+tEnv4000 = atEnv.NETX4000.Clone()
+tEnv4000.Append(CPPPATH = astrIncludePaths)
+tEnv4000.Replace(LDFILE = 'src/netx4000/netx4000.ld')
+tSrc4000 = tEnv4000.SetBuildPath('targets/netx4000', 'src', sources)
+tElf4000 = tEnv4000.Elf('targets/netx4000/apply_hwconfig_netx4000.elf', tSrc4000 + tEnv4000['PLATFORM_LIBRARY'])
+tTxt4000 = tEnv4000.ObjDump('targets/netx4000/apply_hwconfig_netx4000.txt', tElf4000, OBJDUMP_FLAGS=['--disassemble', '--source', '--all-headers', '--wide'])
+tBin4000 = tEnv4000.ObjCopy('targets/netx4000/apply_hwconfig_netx4000.bin', tElf4000)
+tTmp4000 = tEnv4000.GccSymbolTemplate('targets/netx4000/snippet.xml', tElf4000, GCCSYMBOLTEMPLATE_TEMPLATE='targets/hboot_snippet.xml', GCCSYMBOLTEMPLATE_BINFILE=tBin4000[0])
+
+# Create the snippet from the parameters.
+aArtifactGroupReverse4000 = ['com', 'hilscher', 'hw', 'util', 'netx4000']
+atSnippet4000 = {
+    'group': '.'.join(aArtifactGroupReverse4000),
+    'artifact': 'read_rotaryswitch',
+    'version': PROJECT_VERSION,
+    'vcs_id': tEnv4000.Version_GetVcsIdLong(),
+    'vcs_url': tEnv4000.Version_GetVcsUrl(),
+    'license': 'GPL-2.0',
+    'author_name': 'Muhkuh team',
+    'author_url': 'https://github.com/muhkuh-sys',
+    'description': 'Read rotary switches make them available on netX4000.',
+    'categories': ['netx4000', 'booting'],
     'parameter': {
         'CONFIGURATION_ADDRESS': {'help': 'The address of the hardware config.', 'default': 0}
     }
 
 }
-strArtifactPath = 'targets/snippets/%s/%s/%s' % ('/'.join(aArtifactGroupReverse), atSnippet['artifact'], PROJECT_VERSION)
-snippet_netx90_mpw_com = tEnv.HBootSnippet('%s/%s-%s.xml' % (strArtifactPath, atSnippet['artifact'], PROJECT_VERSION), tTmp, PARAMETER=atSnippet)
+strArtifactPath4000 = 'targets/snippets/%s/%s/%s' % ('/'.join(aArtifactGroupReverse4000), atSnippet4000['artifact'], PROJECT_VERSION)
+snippet_netx4000_com = tEnv4000.HBootSnippet('%s/%s-%s.xml' % (strArtifactPath4000, atSnippet4000['artifact'], PROJECT_VERSION), tTmp4000, PARAMETER=atSnippet4000)
 
 # Create the POM file.
-tPOM = tEnv.POMTemplate('%s/%s-%s.pom' % (strArtifactPath, atSnippet['artifact'], PROJECT_VERSION), 'templates/pom.xml', POM_TEMPLATE_GROUP=atSnippet['group'], POM_TEMPLATE_ARTIFACT=atSnippet['artifact'], POM_TEMPLATE_VERSION=atSnippet['version'], POM_TEMPLATE_PACKAGING='xml')
+tPOM4000 = tEnv4000.POMTemplate('%s/%s-%s.pom' % (strArtifactPath4000, atSnippet4000['artifact'], PROJECT_VERSION), 'templates/pom.xml', POM_TEMPLATE_GROUP=atSnippet4000['group'], POM_TEMPLATE_ARTIFACT=atSnippet4000['artifact'], POM_TEMPLATE_VERSION=atSnippet4000['version'], POM_TEMPLATE_PACKAGING='xml')
+
+# Demo
+hboot_netx4000_demo = tEnv4000.HBootImage('targets/hboot_netx4000_demo.bin', 'demo/test_snippet_netx4000_rotary.xml')
--- src/main.c
+++ src/main.c
@@ -1,245 +1,62 @@
 #include "netx_io_areas.h"
 #include "rdy_run.h"
 
-
-/* Define the structures for the hwconfig data. */
-typedef struct NETX90MPW_HWCONFIG_MMIO_STRUCT
+unsigned char getMmioValue(unsigned char mmioIndex)
 {
-	unsigned long aulMMIO[16];
-} NETX90MPW_HWCONFIG_MMIO_T;
-
-typedef struct NETX90MPW_HWCONFIG_PAD_CTRL_STRUCT
-{
-	unsigned long aulPadCtrl[101];
-} NETX90MPW_HWCONFIG_PAD_CTRL_T;
-
-typedef struct NETX90MPW_HWCONFIG_IO_CONFIG_STRUCT
-{
-	unsigned long aulIoConfig[9];
-} NETX90MPW_HWCONFIG_IO_CONFIG_T;
-
-typedef struct NETX90MPW_HWCONFIG_HIF_IO_CTRL_STRUCT
-{
-	unsigned long  ulHif_io_cfg;
-	unsigned long  ulHif_pio_cfg;
-	unsigned long  ulHif_pio_out0;
-	unsigned long  ulHif_pio_out1;
-	unsigned long  ulHif_pio_oe0;
-	unsigned long  ulHif_pio_oe1;
-} NETX90MPW_HWCONFIG_HIF_IO_CTRL_T;
-
-typedef struct NETX90MPW_HWCONFIG_HIF_ASYNCMEM_CTRL_STRUCT
-{
-	unsigned long  ulExtsram0_ctrl;
-	unsigned long  ulExtsram1_ctrl;
-	unsigned long  ulExtsram2_ctrl;
-	unsigned long  ulExtsram3_ctrl;
-	unsigned long  ulExt_cs0_apm_ctrl;
-	unsigned long  ulExt_rdy_cfg;
-} NETX90MPW_HWCONFIG_HIF_ASYNCMEM_CTRL_T;
-
-typedef struct NETX90MPW_HWCONFIG_HIF_SDRAM_CTRL_STRUCT
-{
-	unsigned long  ulSdram_general_ctrl;
-	unsigned long  ulSdram_timing_ctrl;
-	unsigned long  ulSdram_mr;
-} NETX90MPW_HWCONFIG_HIF_SDRAM_CTRL_T;
-
-typedef struct NETX90MPW_HWCONFIG_HIFMEM_PRIORITY_CTRL_STRUCT
-{
-	unsigned long  ulExtmem_prio_timslot_ctrl;
-	unsigned long  ulExtmem_prio_accesstime_ctrl;
-} NETX90MPW_HWCONFIG_HIFMEM_PRIORITY_CTRL_T;
-
-
-typedef struct NETX90MPW_HWCONFIG_STRUCT
-{
-	NETX90MPW_HWCONFIG_MMIO_T tMmio;
-	NETX90MPW_HWCONFIG_PAD_CTRL_T tPadCtrl;
-	NETX90MPW_HWCONFIG_IO_CONFIG_T tIoConfig;
-	NETX90MPW_HWCONFIG_HIF_IO_CTRL_T tHifIoCtrl;
-	NETX90MPW_HWCONFIG_HIF_ASYNCMEM_CTRL_T tHifAsyncmemCtrl;
-	NETX90MPW_HWCONFIG_HIF_SDRAM_CTRL_T tHifSdramCtrl;
-	NETX90MPW_HWCONFIG_HIFMEM_PRIORITY_CTRL_T tHifmemPriorityCtrl;
-	NX90_MLED_CTRL_AREA_T tMledCtrl;
-} NETX90MPW_HWCONFIG_T;
-
-
-static void apply_mmios(const NETX90MPW_HWCONFIG_MMIO_T * const ptCfg)
-{
-	HOSTDEF(ptAsicCtrlArea);
 	HOSTDEF(ptMmioCtrlArea);
-	unsigned int uiCnt;
-	unsigned long ulValue;
-	
-	/* Loop over all MMIO pins and set the complete register. */
-	for(uiCnt=0; uiCnt<16; uiCnt++)
-	{
-		ulValue = ptCfg->aulMMIO[uiCnt];
-		ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
-		ptMmioCtrlArea->aulMmio_cfg[uiCnt] = ulValue;
-	}
+
+	unsigned char ucRegisterIndex;
+	unsigned char ucIndexInRegister;
+	unsigned char ucBitResult;
+	unsigned long ulTemp;
+
+	/* Extract Register Index 0-3*/
+	ucRegisterIndex = (unsigned char)((mmioIndex & 0xe0) >> 5);
+
+	/* Extract Index in register 0-31*/
+	ucIndexInRegister = (unsigned char)(mmioIndex & 0x1f);
+
+	/* Look in extracted register after Index stored in mmioIndex*/
+#if ASIC_TYP==ASIC_TYP_NETX90
+	ulTemp = ptMmioCtrlArea->ulMmio_in_line_status0;
+#elif ASIC_TYP==ASIC_TYP_NETX4000
+	ulTemp = ptMmioCtrlArea->aulMmio_in_line_status[ucRegisterIndex];
+#endif
+	ucBitResult = (ulTemp >> ucIndexInRegister) & 0x1;
+
+	return ucBitResult;
 }
 
+void start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios);
 
-static void apply_mled(const NX90_MLED_CTRL_AREA_T * const ptCfg )
+void __attribute__ ((section (".init_code"))) start(unsigned char *pucResult, unsigned long ulIndexRotaryMmios)
 {
-	HOSTDEF(ptMledCtrlComArea);
 	int i;
-	
-	for (i=0; i<=7; i++) 
-	{
-		ptMledCtrlComArea->aulMled_ctrl_output_sel[i]     = ptCfg->aulMled_ctrl_output_sel[i];
-		ptMledCtrlComArea->aulMled_ctrl_output_on_time[i] = ptCfg->aulMled_ctrl_output_on_time[i];
+	int iEnd;
+	unsigned char ucIdx;
+	unsigned char ucBitValue;
+	unsigned char ucResult;
+
+	ucResult = 0;
+
+#if ASIC_TYP==ASIC_TYP_NETX90
+	iEnd = 1;
+#elif ASIC_TYP==ASIC_TYP_NETX4000
+	iEnd = 4;
+#endif
+
+	for(i=0; i<iEnd; i++) {
+		/* Extract MMIO index of R1 */
+		ucIdx = (unsigned char)((ulIndexRotaryMmios >> (24-(8*i) )) & 0xff);
+
+		/* Check if MMIO is used */
+		if (ucIdx != 0xff)
+		{
+			ucBitValue = getMmioValue(ucIdx);
+			ucResult = (ucResult << 1) | ucBitValue;
+		}
 	}
-	ptMledCtrlComArea->ulMled_ctrl_line0  = ptCfg->ulMled_ctrl_line0;
-	ptMledCtrlComArea->ulMled_ctrl_cfg    = ptCfg->ulMled_ctrl_cfg;
-}
 
-
-static void apply_pad_ctrl(const NETX90MPW_HWCONFIG_PAD_CTRL_T * const ptCfg)
-{
-	HOSTDEF(ptAsicCtrlArea);
-	volatile unsigned long *pulPadCtrl = (volatile unsigned long *)HOSTADDR(pad_ctrl);
-	unsigned int uiCnt;
-	unsigned long ulValue;
-
-
-	/* Loop over all pad controls and set the complete register. */
-	for(uiCnt=0; uiCnt<101; uiCnt++)
-	{
-		ulValue = ptCfg->aulPadCtrl[uiCnt];
-		ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
-		pulPadCtrl[uiCnt] = ulValue;
-	}
-}
-
-
-
-static void apply_io_config(const NETX90MPW_HWCONFIG_IO_CONFIG_T * const ptCfg)
-{
-	HOSTDEF(ptAsicCtrlArea);
-	unsigned int uiCnt;
-	unsigned long ulValue;
-
-
-	/* Loop over all IO configuration register. */
-	for(uiCnt=0; uiCnt<9; uiCnt++)
-	{
-		ulValue = ptCfg->aulIoConfig[uiCnt];
-		ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
-		ptAsicCtrlArea->asIo_config[uiCnt].ulConfig = ulValue;
-	}
-}
-
-
-
-static void apply_hif_io_ctrl(const NETX90MPW_HWCONFIG_HIF_IO_CTRL_T * const ptCfg)
-{
-	HOSTDEF(ptAsicCtrlArea);
-	HOSTDEF(ptHifIoCtrlArea);
-	unsigned long ulValue;
-
-
-	ulValue = ptCfg->ulHif_io_cfg;
-	ptAsicCtrlArea->ulAsic_ctrl_access_key = ptAsicCtrlArea->ulAsic_ctrl_access_key;
-	ptHifIoCtrlArea->ulHif_io_cfg = ulValue;
-
-	ptHifIoCtrlArea->ulHif_pio_cfg = ptCfg->ulHif_pio_cfg;
-	ptHifIoCtrlArea->aulHif_pio_out[0] = ptCfg->ulHif_pio_out0;
-	ptHifIoCtrlArea->aulHif_pio_out[1] = ptCfg->ulHif_pio_out1;
-	ptHifIoCtrlArea->aulHif_pio_oe[0] = ptCfg->ulHif_pio_oe0;
-	ptHifIoCtrlArea->aulHif_pio_oe[1] = ptCfg->ulHif_pio_oe1;
-}
-
-
-typedef struct NX90_HIF_ASYNCMEM_CTRL_AREA_STRUCT
-{
-	volatile unsigned long aulExtsram_ctrl[4];
-	volatile unsigned long  ulExt_cs0_apm_ctrl;
-	volatile unsigned long aulReserved14[3];
-	volatile unsigned long  ulExt_rdy_cfg;
-	volatile unsigned long  ulExt_rdy_status;
-} NX90_HIF_ASYNCMEM_CTRL_AREA_T;
-
-#define NX90_DEF_ptHifAsyncmemCtrlArea NX90_HIF_ASYNCMEM_CTRL_AREA_T * const ptHifAsyncmemCtrlArea = (NX90_HIF_ASYNCMEM_CTRL_AREA_T * const)Addr_NX90_hif_asyncmem_ctrl;
-
-static void apply_hif_asyncmem_ctrl(const NETX90MPW_HWCONFIG_HIF_ASYNCMEM_CTRL_T * const ptCfg)
-{
-	HOSTDEF(ptHifAsyncmemCtrlArea);
-
-
-	ptHifAsyncmemCtrlArea->aulExtsram_ctrl[0] = ptCfg->ulExtsram0_ctrl;
-	ptHifAsyncmemCtrlArea->aulExtsram_ctrl[1] = ptCfg->ulExtsram1_ctrl;
-	ptHifAsyncmemCtrlArea->aulExtsram_ctrl[2] = ptCfg->ulExtsram2_ctrl;
-	ptHifAsyncmemCtrlArea->aulExtsram_ctrl[3] = ptCfg->ulExtsram3_ctrl;
-	ptHifAsyncmemCtrlArea->ulExt_rdy_cfg = ptCfg->ulExt_cs0_apm_ctrl;
-}
-
-
-
-typedef struct NX90_HIF_SDRAM_CTRL_AREA_Ttag
-{
-  volatile unsigned long  ulSdram_general_ctrl;
-  volatile unsigned long  ulSdram_timing_ctrl;
-  volatile unsigned long  ulSdram_mr;
-} NX90_HIF_SDRAM_CTRL_AREA_T;
-
-#define NX90_DEF_ptHifSdramCtrlArea NX90_HIF_SDRAM_CTRL_AREA_T * const ptHifSdramCtrlArea = (NX90_HIF_SDRAM_CTRL_AREA_T * const)Addr_NX90_hif_sdram_ctrl;
-
-static void apply_hif_sdram_ctrl(const NETX90MPW_HWCONFIG_HIF_SDRAM_CTRL_T * const ptCfg)
-{
-	HOSTDEF(ptHifSdramCtrlArea);
-
-
-	/* Deactivate the SDRAM controller. */
-	ptHifSdramCtrlArea->ulSdram_general_ctrl = 0;
-
-	ptHifSdramCtrlArea->ulSdram_timing_ctrl = ptCfg->ulSdram_timing_ctrl;
-	ptHifSdramCtrlArea->ulSdram_mr = ptCfg->ulSdram_mr;
-	ptHifSdramCtrlArea->ulSdram_general_ctrl = ptCfg->ulSdram_general_ctrl;
-}
-
-/* If Ctrl_en is 1, wait until sdram_ready is 1 */
-static void hif_sdram_wait_for_ready(void)
-{
-	HOSTDEF(ptHifSdramCtrlArea);
-	if ((ptHifSdramCtrlArea->ulSdram_general_ctrl & MSK_NX90_sdram_general_ctrl_ctrl_en) != 0)
-	{
-		while ((ptHifSdramCtrlArea->ulSdram_general_ctrl &  MSK_NX90_sdram_general_ctrl_sdram_ready) == 0 ) {}
-	}
-}
-
-
-typedef struct NX90_HIFMEM_PRIORITY_CTRL_AREA_STRUCT
-{
-	volatile unsigned long  ulExtmem_prio_timslot_ctrl;
-	volatile unsigned long  ulExtmem_prio_accesstime_ctrl;
-} NX90_HIFMEM_PRIORITY_CTRL_AREA_T;
-
-#define NX90_DEF_ptHifmemPriorityCtrlArea NX90_HIFMEM_PRIORITY_CTRL_AREA_T * const ptHifmemPriorityCtrlArea = (NX90_HIFMEM_PRIORITY_CTRL_AREA_T * const)Addr_NX90_hifmem_priority_ctrl;
-
-static void apply_hifmem_priority_ctrl(const NETX90MPW_HWCONFIG_HIFMEM_PRIORITY_CTRL_T * const ptCfg)
-{
-	HOSTDEF(ptHifmemPriorityCtrlArea);
-
-
-	ptHifmemPriorityCtrlArea->ulExtmem_prio_timslot_ctrl = ptCfg->ulExtmem_prio_timslot_ctrl;
-	ptHifmemPriorityCtrlArea->ulExtmem_prio_accesstime_ctrl = ptCfg->ulExtmem_prio_accesstime_ctrl;
-}
-
-
-
-void start(const NETX90MPW_HWCONFIG_T * const ptHwconfig);
-void __attribute__ ((section (".init_code"))) start(const NETX90MPW_HWCONFIG_T * const ptHwconfig)
-{
-	apply_mmios( &(ptHwconfig->tMmio) );
-	apply_pad_ctrl( &(ptHwconfig->tPadCtrl) );
-	apply_io_config( &(ptHwconfig->tIoConfig) );
-	apply_hif_io_ctrl( &(ptHwconfig->tHifIoCtrl) );
-	apply_hif_asyncmem_ctrl( &(ptHwconfig->tHifAsyncmemCtrl) );
-	apply_hif_sdram_ctrl( &(ptHwconfig->tHifSdramCtrl) );
-	apply_hifmem_priority_ctrl( &(ptHwconfig->tHifmemPriorityCtrl) );
-	apply_mled( &(ptHwconfig->tMledCtrl) );
-	hif_sdram_wait_for_ready();
+	/* Write the value to the pointer. */
+	*pucResult = ucResult;
 }
